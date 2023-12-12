354bf2a65d202f8021c75c746a153c41e98f5a28
roberth14 <89623447+roberth14@users.noreply.github.com>
2023-12-11 22:24:48 -0500

    Update main.js

--- script/main.js
+++ script/main.js
@@ -1,4 +1,4 @@
-const urlBassic = "https://seguimientoenviosbackend-production-dffe.up.railway.app"
+const urlBassic = "https://seguimientoenviosbackend-production.up.railway.app"
 //"https://seguimientoenviosbackend-production.up.railway.app"
 
 localStorage.setItem("modulo", "empleado")
